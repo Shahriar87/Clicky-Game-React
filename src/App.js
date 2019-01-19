992d077c949e82f45822f16ac1cbb608778db993
Innovator <shahriar.firoz@gmail.com>
2019-01-19 00:55:27 -0600

    Adding ImageCard Child component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ImageCard from "./components/ImageCard";
 import NavTabs from "./components/NavTabs";
 import ScoreTabs from "./components/ScoreTabs";
 import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
 import picture from "./picture.json";
 
 
@@ -46,29 +47,43 @@ class App extends Component {
     if (this.state.score > this.state.topScore) {
       this.setState({ topScore: this.state.score })
     }
+  };
+
+  // ----- Algorithm for picture shuffle
+  shuffleArray = picturesArray => {
+    for (let i = picturesArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [picturesArray[i], picturesArray[j]] = [picturesArray[j], picturesArray[i]];
+    }
+    return picturesArray;
   }
 
-
-
-
-
+  // ----- Render based on current state
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">React Clicky Game!</h1>
         </header>
+        <h3 className="App-intro">
+          <strong>Click an image to begin! If you click same image twice, you lose</strong>
+          <p className="score"><strong>Score: {this.state.score} | TopScore: {this.state.topScore}</strong></p>
+          <p className="message"><strong>{this.state.message}</strong></p>
+        </h3>
+        <Wrapper
+          shakeWrapper={this.state.shakeit}
+          pictures=
+          {this.state.picture.map(pic => (
+            <ImageCard
+              clickPicture={this.clickPicture}
+              id={pic.id}
+              key={pic.id} 
+              name={pic.name}
+              image={pic.image}
+            />
+          ))}
+        />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+// ----- Footer Component
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+    <footer className="footer">
+        <div className="container">
+            <span className="text-muted">Clicky Game!
+      <img src="https://thumbs.gfycat.com/BestMeagerHoki-size_restricted.gif" className="App-logo" alt="logo" />
+            </span>
+        </div>
+    </footer>
+);
+
+export default Footer;
